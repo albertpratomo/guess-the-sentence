07e09597b69aba1b1f2969064c7612e8e60f8337
Moses Budi H <moz.budi.h@gmail.com>
2022-04-18 17:03:56 +0200

    install tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="flex flex-col gap-8">
+    Input your sentence
+
+    <input type="text" v-model="sentence" @keyup.enter="submitSentence"/>
+  </div>
 </template>
+
+<script setup>
+import {ref} from 'vue';
+
+const sentence = ref();
+const sentenceSubmitted = ref(false);
+
+function submitSentence() {
+  sentenceSubmitted.value = true;
+
+  console.log(sentence.value);
+}
+</script>
