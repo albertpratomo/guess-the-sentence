60ae7182c5faa3bff4e6573fb0b65db2c1544d41
Moses Budi H <moz.budi.h@gmail.com>
2022-04-27 18:44:47 +0200

    attempt to fetch sentence from gsheets

--- src/views/Guess.vue
+++ src/views/Guess.vue
@@ -15,13 +15,32 @@
 </template>
 
 <script setup>
+import {GoogleSpreadsheet} from 'google-spreadsheet';
 import SentenceForm from '@/components/SentenceForm.vue';
 import {ref} from 'vue';
 import { useRoute } from 'vue-router';
 
 const {type} = useRoute().params;
 
-const typeInvalid = !['a', 'b', 'c', 'd'].includes(type);
+const sheets = {
+    a: '10KANUVaIq4T8s374upojJHZfPLdJsNuBQIE77rFceRw',
+};
 
-const sentence = ref('i am type ' + type);
+const typeInvalid = !(type in sheets);
+
+async function fetchSentence() {
+    const sheet = new GoogleSpreadsheet(sheets[type]);
+
+    await sheet.useServiceAccountAuth({
+        client_email: import.meta.env.VITE_GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: import.meta.env.VITE_GOOGLE_PRIVATE_KEY,
+    });
+    
+    const sentence = ref('asaws');
+
+    await sheet.loadInfo();
+    console.log(sheet.title);
+}
+
+fetchSentence();
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,36 @@
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 import { fileURLToPath, URL } from 'url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            crypto: 'rollup-plugin-node-polyfills/polyfills/crypto-browserify',
+            util: 'rollup-plugin-node-polyfills/polyfills/util',
+            events: 'rollup-plugin-node-polyfills/polyfills/events',
+        }
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    process: true,
+                    buffer: true
+                }),
+                NodeModulesPolyfillPlugin(),
+            ],
+        },
+    },
+    build: {
+        rollupOptions: {
+            plugins: [
+                rollupNodePolyFill({crypto: true}),
+            ],
+        },
+    },
 })
